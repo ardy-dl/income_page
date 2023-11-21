49007d21ce6c32f6621117b0f449745889b8193d
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-11-21 20:48:49 +0800

    with footer not yet finalized

---
+++ c-income-page.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('openPopupBtn').addEventListener('click', function() {
+    document.getElementById('popupForm').style.display = 'block';
+});
+
+document.getElementById('closePopupBtn').addEventListener('click', function() {
+    document.getElementById('popupForm').style.display = 'none';
+});
